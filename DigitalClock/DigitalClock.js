95fbd1ee84f185ec72095cc07d9864efca2b8240
hadi <hadi@hadi.com>
2025-04-03 02:32:12 +0330

    finish digital clock

--- DigitalClock/DigitalClock.js
+++ DigitalClock/DigitalClock.js
@@ -1,17 +1,22 @@
-function startTime(){
-    const today = new Date()
-    let h = today.getHours()
-    let m = today.getMinutes()
-    let s = today.getSeconds()
-    h = checkTime(h)
-    m = checkTime(m)
-    s = checkTime(s)
-document.getElementById("timer").innerText = `${h}:${m}:${s}`
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  h = checkTime(h);
+  m = checkTime(m);
+  s = checkTime(s);
+  setTimeout(startTime, 1000);
+  document.getElementById("timer").innerText = `${h}:${m}:${s}`;
 }
 
-function checkTime(i){
-if(i<10){i=`0${i}`}
-return i
+function checkTime(i) {
+  if (i < 10) {
+    i = `0${i}`;
+  }
+  return i;
 }
 
-setInterval(startTime , 1000)
+startTime()
+
+
