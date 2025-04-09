2d639239720e0cb771d4ac3993e2bfcce0b1a67d
hadi <hadi@hadi.com>
2025-04-09 09:21:32 +0330

    to do list codding

--- 05-todolist/todolist.js
+++ 05-todolist/todolist.js
@@ -2,12 +2,15 @@ let deleteIcon = document.querySelectorAll(".delete-icon");
 
 // console.log(deleteIcon.length);
 
-deleteIcon[0].addEventListener("click", (e) => {
-  console.log(e.target);
-});
+// deleteIcon[0].addEventListener("click", (e) => {
+//   console.log(e.target);
+// });
 
 for (i = 0; i < deleteIcon.length; i++) {
   deleteIcon[i].addEventListener("click", (e) => {
     console.log(e.target.parentElement.parentElement.remove());
   });
 }
+
+let addBox = document.getElementById("#add-box")
+console.log(addBox)
