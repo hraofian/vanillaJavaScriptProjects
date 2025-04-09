b7e730a8bf4607a5a7122d2a712e767f39a74161
hadi <hadi@hadi.com>
2025-04-09 09:06:32 +0330

    to do list coding

--- 05-todolist/todolist.js
+++ 05-todolist/todolist.js
@@ -2,7 +2,9 @@ let deleteIcon = document.querySelectorAll(".delete-icon");
 
 // console.log(deleteIcon.length);
 
-// deleteIcon[0].addEventListener("click",(e)=>{console.log(e.target)})
+deleteIcon[0].addEventListener("click", (e) => {
+  console.log(e.target);
+});
 
 for (i = 0; i < deleteIcon.length; i++) {
   deleteIcon[i].addEventListener("click", (e) => {
