446bd1263637d93ade6e646cfdc97ab31511f900
hadi <hadi@hadi.com>
2025-04-13 16:27:55 +0330

    to do list 03 codding js ok

--- 07-todolist03/todolist.js
+++ 07-todolist03/todolist.js
@@ -1,5 +1,6 @@
-// crate close button add add    button to li
+// crate close button add add button to li
 let myLi = document.getElementsByTagName("LI");
+let myUl = document.getElementsByTagName("ul")[0];
 
 for (let i = 0; i < myLi.length; i++) {
   let closeSpan = document.createElement("SPAN");
@@ -13,9 +14,36 @@ for (let i = 0; i < myLi.length; i++) {
 // hidden li by clicking on close button
 let closeButton = document.getElementsByClassName("close");
 for (let i = 0; i < closeButton.length; i++) {
-  closeButton[i].onclick = function () {
-    let parentLi = this.parentElement;
-    parentLi.style.display = "none"
-    console.log("hidden")
-  };
+  closeButton[i].addEventListener("click", () => {
+    closeButton[i].parentElement.style.display = "none";
+  });
 }
+
+//add new task
+
+let addInput = document.getElementById("addInput");
+let addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", (e) => {
+  if (addInput.value !== "") {
+    let inputValue = addInput.value;
+    let inputValueTextNode = document.createTextNode(inputValue);
+    let newLi = document.createElement("li");
+    newLi.appendChild(inputValueTextNode);
+    myUl.appendChild(newLi);
+
+    let closeSpan = document.createElement("SPAN");
+    let closeText = document.createTextNode("x");
+    closeSpan.appendChild(closeText);
+    closeSpan.className = "close";
+    newLi.appendChild(closeSpan);
+
+    closeSpan.addEventListener("click", () => {
+      newLi.style.display = "none";
+    });
+
+    addInput.value = "";
+  } else {
+    return;
+  }
+});
