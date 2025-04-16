0b2ba6c3c76f53680654831c9466a682c567d31c
hadi <hadi@hadi.com>
2025-04-16 13:23:16 +0330

    tip cal codding js ok

--- 08-tipcalculator/tipcalculator.js
+++ 08-tipcalculator/tipcalculator.js
@@ -1,21 +1,20 @@
 let amountInput = document.getElementById("am-input");
 let tipInput = document.getElementById("tip-input");
+let personInput = document.getElementById("person-input");
 let calBtn = document.getElementById("calculateTips");
-let totalResultSpan = document.getElementById("total-result")
-let tipResultSpan = document.getElementById("tip-result")
-
-
+let totalResultSpan = document.getElementById("total-result");
+let tipResultSpan = document.getElementById("tip-result");
 
 function claculate() {
-  let amountValue = parseInt(amountInput.value)
-  let tipValue = parseInt(tipInput.value)
+  let amountValue = parseInt(amountInput.value);
+  let tipValue = parseInt(tipInput.value);
+  let personValue = parseInt(personInput.value);
   tipValue = tipValue * 0.01;
-  let totalResult = amountValue * tipValue
-  totalResultSpan.innerHTML = `${totalResult} $`
-//   let tipResult = totalResult 
-  
+  let totalResult = amountValue * tipValue;
+  totalResultSpan.innerHTML = `${totalResult} $`;
+  tipResult = totalResult / personValue;
+  tipResultSpan.innerHTML = `${tipResult} $`;
 }
-
 
 calBtn.addEventListener("click", () => {
   claculate();
