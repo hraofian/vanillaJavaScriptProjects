4b8502b14d4348a820edb334943f8eefbce0198b
hadi <hadi@hadi.com>
2025-04-16 20:08:55 +0330

    tip cal codding js ok

--- 08-tipcalculator/tipcalculator.js
+++ 08-tipcalculator/tipcalculator.js
@@ -11,36 +11,72 @@ let perBtn10 = document.getElementById("per-10");
 let perBtn15 = document.getElementById("per-15");
 let perBtn20 = document.getElementById("per-20");
 
-perBtn0.addEventListener("click", () => {
+function perBtn0Click() {
   tipInput.value = "0";
   perBtn0.style.backgroundColor = "#023aa3";
   perBtn10.style.backgroundColor = "#0058fa";
   perBtn15.style.backgroundColor = "#0058fa";
   perBtn20.style.backgroundColor = "#0058fa";
-});
+}
 
-perBtn10.addEventListener("click", () => {
+function perBtn10Click() {
   tipInput.value = "10";
   perBtn0.style.backgroundColor = "#0058fa";
   perBtn10.style.backgroundColor = "#023aa3";
   perBtn15.style.backgroundColor = "#0058fa";
   perBtn20.style.backgroundColor = "#0058fa";
-});
+}
 
-perBtn15.addEventListener("click", () => {
+function perBtn15Click() {
   tipInput.value = "15";
   perBtn0.style.backgroundColor = "#0058fa";
   perBtn10.style.backgroundColor = "0058fa";
   perBtn15.style.backgroundColor = "#023aa3";
   perBtn20.style.backgroundColor = "#0058fa";
-});
+}
 
-perBtn20.addEventListener("click", () => {
+function perBtn20Click() {
   tipInput.value = "20";
   perBtn0.style.backgroundColor = "#0058fa";
   perBtn10.style.backgroundColor = "#0058fa";
   perBtn15.style.backgroundColor = "#0058fa";
   perBtn20.style.backgroundColor = "#023aa3";
+}
+function perBtnClickReset() {
+  perBtn0.style.backgroundColor = "#0058fa";
+  perBtn10.style.backgroundColor = "#0058fa";
+  perBtn15.style.backgroundColor = "#0058fa";
+  perBtn20.style.backgroundColor = "#0058fa";
+}
+
+perBtn0.addEventListener("click", () => {
+  perBtn0Click();
+});
+
+perBtn10.addEventListener("click", () => {
+  perBtn10Click();
+});
+
+perBtn15.addEventListener("click", () => {
+  perBtn15Click();
+});
+
+perBtn20.addEventListener("click", () => {
+  perBtn20Click();
+});
+
+tipInput.addEventListener("input", () => {
+  if (tipInput.value === "0") {
+    perBtn0Click();
+  } else if (tipInput.value === "10") {
+    perBtn10Click();
+  } else if (tipInput.value === "15") {
+    perBtn15Click();
+  } else if (tipInput.value === "20") {
+    perBtn20Click();
+  } else {
+    perBtnClickReset();
+  }
 });
 
 function claculate() {
