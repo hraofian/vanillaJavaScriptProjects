b4dac4ae364da75ecf1579e0b437a500d01b8758
hadi <hadi@hadi.com>
2025-04-16 19:46:09 +0330

    tip cal codding tipbtn coding ok

--- 08-tipcalculator/tipcalculator.js
+++ 08-tipcalculator/tipcalculator.js
@@ -5,6 +5,44 @@ let calBtn = document.getElementById("calculateTips");
 let totalResultSpan = document.getElementById("total-result");
 let tipResultSpan = document.getElementById("tip-result");
 
+//percentage butoon
+let perBtn0 = document.getElementById("per-0");
+let perBtn10 = document.getElementById("per-10");
+let perBtn15 = document.getElementById("per-15");
+let perBtn20 = document.getElementById("per-20");
+
+perBtn0.addEventListener("click", () => {
+  tipInput.value = "0";
+  perBtn0.style.backgroundColor = "#023aa3";
+  perBtn10.style.backgroundColor = "#0058fa";
+  perBtn15.style.backgroundColor = "#0058fa";
+  perBtn20.style.backgroundColor = "#0058fa";
+});
+
+perBtn10.addEventListener("click", () => {
+  tipInput.value = "10";
+  perBtn0.style.backgroundColor = "#0058fa";
+  perBtn10.style.backgroundColor = "#023aa3";
+  perBtn15.style.backgroundColor = "#0058fa";
+  perBtn20.style.backgroundColor = "#0058fa";
+});
+
+perBtn15.addEventListener("click", () => {
+  tipInput.value = "15";
+  perBtn0.style.backgroundColor = "#0058fa";
+  perBtn10.style.backgroundColor = "0058fa";
+  perBtn15.style.backgroundColor = "#023aa3";
+  perBtn20.style.backgroundColor = "#0058fa";
+});
+
+perBtn20.addEventListener("click", () => {
+  tipInput.value = "20";
+  perBtn0.style.backgroundColor = "#0058fa";
+  perBtn10.style.backgroundColor = "#0058fa";
+  perBtn15.style.backgroundColor = "#0058fa";
+  perBtn20.style.backgroundColor = "#023aa3";
+});
+
 function claculate() {
   let amountValue = parseInt(amountInput.value);
   let tipValue = parseInt(tipInput.value);
