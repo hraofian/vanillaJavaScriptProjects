4b82c518f9243a60f646c8d455cb381c49ff0a2c
hadi <hadi@hadi.com>
2025-04-17 10:25:10 +0330

    simplest image slider 01 codding finished

--- 10-imageslider/imageslider.js
+++ 10-imageslider/imageslider.js
@@ -1,1 +1,8 @@
-let mainImage = document.getElementById("main-image")
+let mainImage = document.getElementById("main-image");
+let imageDiv = document.getElementById("image-slide-div");
+console.log(mainImage.attributes.src.value);
+for (let i = 0; i < imageDiv.children.length; i++) {
+  imageDiv.children[i].addEventListener("click", () => {
+    mainImage.attributes.src.value = imageDiv.children[i].currentSrc;
+  });
+}
